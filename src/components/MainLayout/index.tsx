8b1ac0c0fb20ce2b8b79e2cf890ed2c02d341cd1
jmj <wjsdncl2222@gmail.com>
2024-06-24 22:48:43 +0900

    ✨ feat(#31): Layout 임시 설정

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 
-import Header from '../Header';
-import Sidebar from '../Sidebar';
+// import Header from '../Header';
+// import Sidebar from '../Sidebar';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
@@ -14,10 +14,15 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <div className='flex max-h-dvh max-w-screen-lg'>
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <aside className='flex h-screen w-72 flex-col gap-14 border-r border-gray_d9 px-4 py-8'>
+        <h1 className='text-xl font-bold'>Sidebar</h1>
+      </aside>
       <div className='flex grow flex-col'>
-        {/* 임시 헤더 */}
-        <Header />
+        {/* <Header /> */}
+        <header className='flex items-center justify-between border-b border-gray_d9 px-6 pb-16 pt-3'>
+          <h1 className='text-xl font-bold'>Header</h1>
+        </header>
         <main className='flex flex-col overflow-y-scroll px-6 py-5'>{children}</main>
       </div>
     </div>
