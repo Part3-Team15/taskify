893643e78c7c68ab4fe3bca95f15eff6c164b900
Jiyun Kim <wayandway@kakao.com>
2024-06-24 16:53:45 +0900

    🛠  fix(#54): 캐시 키를 파라미터로 지정할 수 있도록 수정

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,13 +1,13 @@
-import { useQuery } from 'react-query';
+import { useQuery, QueryKey } from 'react-query';
 
-const useFetchData = <T>(getService: () => Promise<{ data: T }>) => {
+const useFetchData = <T>(queryKey: QueryKey, getService: () => Promise<{ data: T }>) => {
   const fetchData = async () => {
     const response = await getService();
     return response.data;
   };
 
   return useQuery<T, Error>({
-    queryKey: 'data', // 캐시 키
+    queryKey, // 캐시 키를 임의로 지정할 수 있음
     queryFn: fetchData, // 비동기 getService 함수 (services/ 에 정의)
     onError: (error: Error) => {
       console.error('Error fetching data:', error);
