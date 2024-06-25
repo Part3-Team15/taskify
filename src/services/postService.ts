5f1365209f9041cc68c5aa21cceaff130fb90662
Jiyun Kim <wayandway@kakao.com>
2024-06-26 02:10:36 +0900

    ♻️  refactor(#76): postSignIn과 연동한 useSignin 리팩토링

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,8 +1,16 @@
 import instance from './axios';
 
+import { SignInForm, SignInResponse } from '@/types/post/SignInForm.interface';
 import { SignUpForm } from '@/types/post/SignUpForm.interface';
 
 // 회원가입
 export const postSignUp = async (formData: SignUpForm) => {
   return await instance.post(`/users`, formData);
+};
+
+// 로그인
+export const postSignIn = async (formData: SignInForm): Promise<SignInResponse> => {
+  const response = await instance.post<SignInResponse>(`/auth/login`, formData);
+  localStorage.setItem('accessToken', response.data.accessToken);
+  return response.data;
 };
