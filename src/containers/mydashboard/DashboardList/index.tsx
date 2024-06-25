f12bbcec378a84c5023252874d80fd6fccfed877
jmj <wjsdncl2222@gmail.com>
2024-06-25 18:51:34 +0900

    🛠  fix(#16): mydashboard 페이지 대시보드 목록 페이지 수정 - 기존 전역 대시보드 목록을 받아오던 방식을 서버에 요청하여 받아오도록 수정 - 페이지네이션 기능 추가

--- src/containers/mydashboard/DashboardList/index.tsx
+++ src/containers/mydashboard/DashboardList/index.tsx
@@ -1,46 +1,46 @@
+/* eslint-disable no-console */
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
 
 import arrow from '@/../public/icons/arrow-right.svg';
 import crown from '@/../public/icons/crown.svg';
 import plus from '@/../public/icons/plus-filled.svg';
-import { RootState } from '@/store/store';
-import { Dashboard } from '@/types/Dashboard.interface';
+import { getDashboardsList } from '@/services/getService';
+import { DashboardsResponse } from '@/types/Dashboard.interface';
 
 export default function DashboardList() {
-  const dashboards = useSelector((state: RootState) => state.dashboards.dashboards);
-  const [currentChunk, setCurrentChunk] = useState(0);
-  const [currentDashboards, setCurrentDashboards] = useState<Dashboard[]>([]);
+  const [dashboardResponse, setDashboardResponse] = useState<DashboardsResponse>();
+  const [currentChunk, setCurrentChunk] = useState(1);
 
-  // 대시보드 배열을 일정한 크기의 청크로 나누는 함수
-  const chunkArray = (array: Dashboard[], size: number): Dashboard[][] => {
-    const chunkedArr: Dashboard[][] = [];
-    for (let i = 0; i < array.length; i += size) {
-      chunkedArr.push(array.slice(i, i + size));
-    }
-    return chunkedArr;
-  };
-
-  // 이중 배열 생성
-  const dashboardChunks = chunkArray(dashboards, 5);
+  useEffect(() => {
+    const getDashboardList = async () => {
+      try {
+        const response = await getDashboardsList('pagination', currentChunk, 5);
+        setDashboardResponse(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getDashboardList();
+  }, [currentChunk]);
 
   const handleNext = () => {
-    if (currentChunk < dashboardChunks.length - 1) {
-      setCurrentChunk(currentChunk + 1);
+    const nextChunk = currentChunk + 1;
+
+    if (dashboardResponse && nextChunk <= Math.ceil(dashboardResponse.totalCount / 5)) {
+      setCurrentChunk(nextChunk);
     }
   };
 
   const handlePrev = () => {
-    if (currentChunk > 0) {
-      setCurrentChunk(currentChunk - 1);
+    const prevChunk = currentChunk - 1;
+
+    if (prevChunk >= 1) {
+      setCurrentChunk(prevChunk);
     }
   };
-
-  useEffect(() => {
-    setCurrentDashboards(dashboardChunks[currentChunk] || []);
-  }, [currentChunk, dashboards]);
 
   return (
     <section className=''>
@@ -51,7 +51,7 @@ export default function DashboardList() {
             <Image src={plus} alt='plus' />
           </button>
         </li>
-        {currentDashboards.map((dashboard) => (
+        {dashboardResponse?.dashboards.map((dashboard) => (
           <li className='h-16 max-w-80 rounded-lg border border-gray-d9' key={dashboard.id}>
             <Link
               href={`/dashboard/${dashboard.id}`}
@@ -67,7 +67,7 @@ export default function DashboardList() {
 
       <div className='flex items-center justify-end pt-3'>
         <span className='pr-4 text-sm text-black-33'>
-          {dashboardChunks.length} 페이지 중 {currentChunk + 1}
+          {dashboardResponse ? Math.ceil(dashboardResponse.totalCount / 5) : 1} 페이지 중 {currentChunk}
         </span>
         <button
           className='flex size-10 items-center justify-center rounded-s-[4px] border border-gray-d9'
