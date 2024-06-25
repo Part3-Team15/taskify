239e813e89db33ff4cc7eb94efae050e81fb9ae8
jmj <wjsdncl2222@gmail.com>
2024-06-25 21:25:48 +0900

    🎨 design(#16): 대시보드 이동 버튼 화살표 추가 - section 크기 조정

--- src/containers/mydashboard/DashboardList/index.tsx
+++ src/containers/mydashboard/DashboardList/index.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 
-import arrow from '@/../public/icons/arrow-right.svg';
+import arrowWhite from '@/../public/icons/arrow-white.svg';
+import arrowBlack from '@/../public/icons/arrow-black.svg';
 import crown from '@/../public/icons/crown.svg';
 import plus from '@/../public/icons/plus-filled.svg';
 import useFetchData from '@/hooks/useFetchData';
@@ -45,23 +46,26 @@ export default function DashboardList() {
   };
 
   return (
-    <section className=''>
+    <section className='w-max'>
       <ul className='text-black-33 grid min-h-[140px] grid-cols-3 gap-3 font-semibold'>
-        <li className='border-gray-d9 h-16 max-w-80 rounded-lg border'>
+        <li className='border-gray-d9 h-16 w-80 rounded-lg border'>
           <button className='hover:bg-violet-f1 flex size-full items-center justify-center gap-4'>
             새로운 대시보드
             <Image src={plus} alt='plus' />
           </button>
         </li>
         {dashboardResponse?.dashboards.map((dashboard) => (
-          <li className='border-gray-d9 h-16 max-w-80 rounded-lg border' key={dashboard.id}>
+          <li className='border-gray-d9 h-16 w-80 rounded-lg border' key={dashboard.id}>
             <Link
               href={`/dashboard/${dashboard.id}`}
               className={'hover:bg-violet-f1 flex size-full items-center rounded-md px-5'}
             >
-              <div className='rounded-full p-1' style={{ backgroundColor: dashboard.color }} />
-              <p className='pl-4 pr-1 text-lg font-medium'>{dashboard.title}</p>
-              {dashboard.createdByMe && <Image src={crown} alt='my' />}
+              <div className='flex size-full items-center'>
+                <div className='rounded-full p-1' style={{ backgroundColor: dashboard.color }} />
+                <p className='pl-4 pr-1 text-lg font-medium'>{dashboard.title}</p>
+                {dashboard.createdByMe && <Image src={crown} alt='my' />}
+              </div>
+              <Image src={arrowBlack} alt='arrow' />
             </Link>
           </li>
         ))}
@@ -75,13 +79,13 @@ export default function DashboardList() {
           className='border-gray-d9 flex size-10 items-center justify-center rounded-s-[4px] border'
           onClick={handlePrev}
         >
-          <Image src={arrow} alt='arrow-left' className='rotate-180' />
+          <Image src={arrowWhite} alt='arrow-left' className='rotate-180' />
         </button>
         <button
           className='border-gray-d9 flex size-10 items-center justify-center rounded-e-[4px] border'
           onClick={handleNext}
         >
-          <Image src={arrow} alt='arrow-right' />
+          <Image src={arrowWhite} alt='arrow-right' />
         </button>
       </div>
     </section>
