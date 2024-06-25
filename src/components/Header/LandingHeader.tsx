b0ad2d221860b23c9c42f0ea0aff39cc717767f7
un0211 <un0211@kaist.ac.kr>
2024-06-25 09:49:19 +0900

    🚚 rename(#9): 이미지 이름 - 사용하도록 변경

--- src/components/Header/LandingHeader.tsx
+++ src/components/Header/LandingHeader.tsx
@@ -5,7 +5,7 @@ export default function LandingHeader() {
   return (
     <header className='flex h-[70px] w-full items-center justify-between bg-black px-[24px]'>
       <Link href='/'>
-        <Image src='/icons/logo_white.svg' alt='로고' width={121} height={39} />
+        <Image src='/icons/logo-white.svg' alt='로고' width={121} height={39} />
       </Link>
       <div className='flex gap-5 text-sm text-white md:gap-9 md:text-base'>
         <Link href='/signin'>로그인</Link>
