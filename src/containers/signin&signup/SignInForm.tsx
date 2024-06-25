4b82288d1ac878e6994f860fae44715f9cf3a3cb
Daeyang lee <eodid007@gmail.com>
2024-06-25 13:33:29 +0900

    ⚙️  chore(#72): eslint tui 제거

--- src/containers/signin&signup/SignInForm.tsx
+++ src/containers/signin&signup/SignInForm.tsx
@@ -20,7 +20,6 @@ const schema = yup.object().shape({
   password: yup.string().required('비밀번호를 입력해주세요.').min(8, '비밀번호는 최소 8자 이상이어야 합니다.'),
 });
 
-// eslint-disable-next-line complexity
 export default function SignInForm() {
   const [checkTerms, setCheckTerms] = useState(false);
 
