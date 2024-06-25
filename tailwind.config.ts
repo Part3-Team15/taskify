7c68a37d838a68da9fe136606478c08bc6aacece
Daeyang lee <eodid007@gmail.com>
2024-06-25 14:28:57 +0900

    ⚙️  chore(#74): tailwind.config.ts 컬러 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,36 +14,53 @@ const config: Config = {
       lg: '1280px',
     },
     colors: {
-      black: '#000000',
-      black_17: '#171717',
-      black_33: '#333236',
-      black_4b: '#4b4b4b',
-      gray_78: '#787486',
-      gray_9f: '#9fa6b2',
-      gray_d9: '#d9d9d9',
-      gray_ee: '#eeeeee',
-      gray_fa: '#fafafa',
+      black: {
+        DEFAULT: '#000000',
+        17: '#171717',
+        33: '#333236',
+        '4b': '#4b4b4b',
+      },
+      gray: {
+        78: '#787486',
+        '9f': '#9fa6b2',
+        d9: '#d9d9d9',
+        ee: '#eeeeee',
+        fa: '#fafafa',
+      }, 
       white: '#ffffff',
-      violet: '#5534da',
-      violet_f1: '#f1effd',
-      violet_hover: '#3925a8',
-      red: '#D6173A',
-      red_hover: '#b51230',
-      green: '#7ac555',
-      purple: '#760dde',
-      orange: '#ffa500',
-      blue: '#76a5ea',
-      pink: '#e876ea',
-      
+      violet: {
+        DEFAULT: '#5534da',
+        f1: '#f1effd',
+        hover: '#3925a8',
+      },
+      red: {
+        DEFAULT: '#D6173A',
+        red_hover: '#b51230',
+      }, 
+      green: {
+        DEFAULT: '#7ac555',
+      },
+      purple: {
+        DEFAULT: '#760dde',
+      },
+      orange: {
+        DEFAULT: '#ffa500',
+      },
+      blue: {
+        DEFAULT: '#76a5ea',
+      },
+      pink: {
+        DEFAULT: '#e876ea',
+      },
     },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
