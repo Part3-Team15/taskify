ea7b970c840706a379182d80954b4349626534db
Daeyang lee <eodid007@gmail.com>
2024-06-26 17:06:11 +0900

    ✨ feat(#94, #42): 공통 모달 알림창 구현

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import DefaultModal from './DefaultModal';
 import InviteMemberModal from './InviteMemberModal';
 import NewColumnModal from './NewColumnModal';
+import NotificationModal from './NotificationModal';
 
 import { modalSelector, closeModal } from '@/store/reducers/modalSlice';
 
@@ -20,8 +21,23 @@ export default function Modal() {
     }
   };
 
+  const notificationTextObj = {
+    pwdNotEqual: '비밀번호가 일치하지 않습니다.',
+    signupSuccess: '가입이 완료되었습니다!',
+    emailExists: '이미 사용 중인 이메일입니다.',
+    curPwdNotEqual: '현재 비밀번호가 틀렸습니다.',
+  };
+
   const renderModalContent = () => {
     switch (type) {
+      case 'pwdNotEqual':
+        return <NotificationModal handleCloseModal={handleCloseModal} notificationText={notificationTextObj[type]} />;
+      case 'signupSuccess':
+        return <NotificationModal handleCloseModal={handleCloseModal} notificationText={notificationTextObj[type]} />;
+      case 'emailExists':
+        return <NotificationModal handleCloseModal={handleCloseModal} notificationText={notificationTextObj[type]} />;
+      case 'curPwdNotEqual':
+        return <NotificationModal handleCloseModal={handleCloseModal} notificationText={notificationTextObj[type]} />;
       case 'newColumn':
         return <NewColumnModal handleCloseModal={handleCloseModal} />;
       case 'inviteMember':
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,55 @@
-const Home: React.FC = () => <div>Home</div>;
+import useModal from '@/hooks/useModal';
+
+const Home: React.FC = () => {
+  const { openModal } = useModal();
+
+  return (
+    <div>
+      Home{' '}
+      <div className='flex gap-[10px]'>
+        <button
+          className='btn-violet h-[40px] w-[100px]'
+          onClick={() => {
+            openModal({ type: 'inviteMember', props: null });
+          }}
+        >
+          모달 열기
+        </button>
+        <button
+          className='btn-violet h-[40px] w-[125px]'
+          onClick={() => {
+            openModal({ type: 'pwdNotEqual', props: null });
+          }}
+        >
+          비밀번호 불일치
+        </button>
+        <button
+          className='btn-violet h-[40px] w-[125px]'
+          onClick={() => {
+            openModal({ type: 'signupSuccess', props: null });
+          }}
+        >
+          가입 완료
+        </button>
+        <button
+          className='btn-violet h-[40px] w-[125px]'
+          onClick={() => {
+            openModal({ type: 'emailExists', props: null });
+          }}
+        >
+          이메일 중복
+        </button>
+        <button
+          className='btn-violet h-[40px] w-[125px]'
+          onClick={() => {
+            openModal({ type: 'curPwdNotEqual', props: null });
+          }}
+        >
+          현재 비번 틀림
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
