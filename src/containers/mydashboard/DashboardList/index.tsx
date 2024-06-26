cac641e8a79eda548d46554a4c90afef2bbde849
jmj <wjsdncl2222@gmail.com>
2024-06-26 10:45:16 +0900

    🛠  fix(#16): 페이지네이션 버튼 분리

--- src/containers/mydashboard/DashboardList/index.tsx
+++ src/containers/mydashboard/DashboardList/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
@@ -47,7 +46,7 @@ export default function DashboardList() {
 
   return (
     <section className='w-max'>
-      <ul className='grid gap-3 font-semibold text-black-33 md:min-h-[215px] md:grid-cols-2 lg:min-h-[140px] lg:grid-cols-3'>
+      <ul className='grid gap-3 font-semibold text-black-33 md:min-h-[216px] md:grid-cols-2 lg:min-h-[140px] lg:grid-cols-3'>
         <li className='h-16 w-64 rounded-lg border border-gray-d9 bg-white md:w-60 lg:w-80'>
           <button className='flex size-full items-center justify-center gap-4 hover:bg-violet-f1'>
             새로운 대시보드
@@ -71,23 +70,28 @@ export default function DashboardList() {
         ))}
       </ul>
 
-      <div className='flex items-center justify-end pt-3'>
+      <div className='rou flex items-center justify-end pt-3'>
         <span className='pr-4 text-sm text-black-33'>
           {dashboardResponse ? Math.ceil(dashboardResponse.totalCount / 5) : 1} 페이지 중 {currentChunk}
         </span>
-        <button
-          className='flex size-10 items-center justify-center rounded-s-[4px] border border-gray-d9'
-          onClick={handlePrev}
-        >
-          <Image src={arrowWhite} alt='arrow-left' className='rotate-180' />
-        </button>
-        <button
-          className='flex size-10 items-center justify-center rounded-e-[4px] border border-gray-d9'
-          onClick={handleNext}
-        >
-          <Image src={arrowWhite} alt='arrow-right' />
-        </button>
+
+        <NavButton direction='left' onClick={handlePrev} />
+        <NavButton direction='right' onClick={handleNext} />
       </div>
     </section>
   );
 }
+
+interface NavButtonProps {
+  direction: 'left' | 'right';
+  onClick: () => void;
+}
+
+const NavButton = ({ direction, onClick }: NavButtonProps) => (
+  <button
+    className={`${direction === 'left' ? 'rounded-s-[4px]' : 'rounded-e-[4px]'} flex size-10 items-center justify-center border border-gray-d9 hover:bg-gray-ee`}
+    onClick={onClick}
+  >
+    <Image src={arrowWhite} alt={`arrow-${direction}`} className={`${direction === 'left' ? 'rotate-180' : ''}`} />
+  </button>
+);
