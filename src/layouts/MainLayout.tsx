4a00a143a174b13f1ac7c7fb8e0cb862ad342a55
un0211 <un0211@kaist.ac.kr>
2024-06-26 22:44:20 +0900

    🎨 design(#31): 레이아웃 main이 남은 크기 차지하도록 설정

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -21,12 +21,12 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   if (isDisabled) return <div className='min-w-[360px]'>{children}</div>;
 
   return (
-    <div className='flex min-w-[360px]'>
+    <div className='flex min-h-lvh min-w-[360px]'>
       <Sidebar />
 
       <div className='flex grow flex-col'>
         <Header />
-        <main className='flex flex-col overflow-y-scroll bg-gray-fa'>{children}</main>
+        <main className='flex grow flex-col overflow-y-scroll bg-gray-fa'>{children}</main>
       </div>
     </div>
   );
