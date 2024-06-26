edbd165592f98c8a4f487b4e0aaf838651d133b9
jmj <wjsdncl2222@gmail.com>
2024-06-26 13:14:25 +0900

    🛠  fix(#2): API 로딩 시 UI 수정

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -17,30 +17,34 @@ export default function Sidebar() {
   const { isLoading } = useFetchDashboards();
   const dashboards = useSelector((state: RootState) => state.dashboards.dashboards);
 
-  if (isLoading) return <div>Loading...</div>;
-
   return (
     <aside className='flex h-screen min-w-72 flex-col gap-14 border-r border-gray-d9 px-3 py-5'>
-      <Link href='/' className='px-3'>
-        <Image src={logo} alt='logo' priority />
-      </Link>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <Link href='/' className='px-3'>
+            <Image src={logo} alt='logo' priority />
+          </Link>
 
-      <div className='flex flex-col gap-2'>
-        <div className='flex items-center justify-between'>
-          <p className='px-3 text-xs font-bold text-gray-78'>Dashboards</p>
+          <div className='flex flex-col gap-2'>
+            <div className='flex items-center justify-between'>
+              <p className='px-3 text-xs font-bold text-gray-78'>Dashboards</p>
 
-          {/* 모달 연결 해야함 */}
-          <a href='#' className='p-3'>
-            <Image src={plus} alt='add' />
-          </a>
-        </div>
-        <div className='mx-2 mb-2 border-b border-gray-d9' />
-        <ul className='flex flex-col gap-2'>
-          {dashboards.map((dashboard) => (
-            <DashboardItem key={dashboard.id} dashboard={dashboard} nowDashboard={Number(id)} />
-          ))}
-        </ul>
-      </div>
+              {/* 모달 연결 해야함 */}
+              <a href='#' className='p-3'>
+                <Image src={plus} alt='add' />
+              </a>
+            </div>
+            <div className='mx-2 mb-2 border-b border-gray-d9' />
+            <ul className='flex flex-col gap-2'>
+              {dashboards.map((dashboard) => (
+                <DashboardItem key={dashboard.id} dashboard={dashboard} nowDashboard={Number(id)} />
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </aside>
   );
 }
