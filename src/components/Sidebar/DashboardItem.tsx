ff4544810051e594b6e5d3d0a6102ef45dba3608
JMJ <89517903+wjsdncl@users.noreply.github.com>
2024-06-26 15:34:41 +0900

    ✨ Feat: SideBar 컴포넌트 반응형 디자인 추가 및 로딩 UI 수정 (#92)
    * 🛠  fix(#2): API 로딩 시 UI 수정
    
    * 🎨 design(#2): 반응형 디자인 설정
    
    * 🎨 design(#2): 모바일용 로고 이미지 파일 업로드
    
    * 🎨 design(#2): 이미지를 불러오는 방식 수정

--- src/components/Sidebar/DashboardItem.tsx
+++ src/components/Sidebar/DashboardItem.tsx
@@ -11,14 +11,16 @@ interface DashboardItemProps {
 
 export default function DashboardItem({ dashboard, nowDashboard }: DashboardItemProps) {
   const isActive = nowDashboard === dashboard.id;
-  const itemClasses = `flex items-center gap-4 rounded-md px-3 py-3 ${isActive ? 'bg-violet-f1 text-black-33' : 'text-gray-78'} hover:bg-violet-f1`;
+  const itemClasses = `flex items-center justify-center md:justify-start rounded-md py-3 md:px-3 ${isActive ? 'bg-violet-f1 text-black-33' : 'text-gray-78'} hover:bg-violet-f1`;
 
   return (
     <li>
       <Link href={`/dashboard/${dashboard.id}`} className={itemClasses}>
         <div className='rounded-full p-1' style={{ backgroundColor: dashboard.color }} />
-        <p className='text-lg font-medium'>{dashboard.title}</p>
-        {dashboard.createdByMe && <Image src={crown} alt='my' />}
+        <p className='hidden pl-4 pr-[6px] text-lg font-medium md:block'>{dashboard.title}</p>
+        {dashboard.createdByMe && (
+          <Image src={'/icons/crown.svg'} alt='my' className='hidden md:block' width={18} height={14} />
+        )}
       </Link>
     </li>
   );
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,7 +5,6 @@ import { useSelector } from 'react-redux';
 
 import DashboardItem from './DashboardItem';
 
-import logo from '@/../public/icons/logo.svg';
 import plus from '@/../public/icons/plus.svg';
 import { useFetchDashboards } from '@/hooks/useFetchDashboards';
 import { RootState } from '@/store/store';
@@ -17,30 +16,42 @@ export default function Sidebar() {
   const { isLoading } = useFetchDashboards();
   const dashboards = useSelector((state: RootState) => state.dashboards.dashboards);
 
-  if (isLoading) return <div>Loading...</div>;
-
   return (
-    <aside className='flex h-screen min-w-72 flex-col gap-14 border-r border-gray-d9 px-3 py-5'>
-      <Link href='/' className='px-3'>
-        <Image src={logo} alt='logo' priority />
-      </Link>
+    <aside className='flex h-screen min-w-16 flex-col gap-14 border-r border-gray-d9 px-3 py-5 md:min-w-40 lg:min-w-72'>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <Link href='/' className='flex items-center justify-center md:block md:px-3'>
+            <Image src={'/icons/logo.svg'} alt='logo' priority className='hidden md:block' width={110} height={33} />
+            <Image
+              src={'/icons/logo-small.svg'}
+              alt='logo'
+              priority
+              className='block md:hidden'
+              width={27}
+              height={27}
+            />
+          </Link>
 
-      <div className='flex flex-col gap-2'>
-        <div className='flex items-center justify-between'>
-          <p className='px-3 text-xs font-bold text-gray-78'>Dashboards</p>
+          <div className='flex flex-col gap-2'>
+            <div className='flex items-center justify-center md:justify-between'>
+              <p className='hidden px-3 text-xs font-bold text-gray-78 md:block'>Dashboards</p>
 
-          {/* 모달 연결 해야함 */}
-          <a href='#' className='p-3'>
-            <Image src={plus} alt='add' />
-          </a>
-        </div>
-        <div className='mx-2 mb-2 border-b border-gray-d9' />
-        <ul className='flex flex-col gap-2'>
-          {dashboards.map((dashboard) => (
-            <DashboardItem key={dashboard.id} dashboard={dashboard} nowDashboard={Number(id)} />
-          ))}
-        </ul>
-      </div>
+              {/* 모달 연결 해야함 */}
+              <a href='#' className='p-3'>
+                <Image src={'/icons/plus.svg'} alt='add' width={15} height={15} />
+              </a>
+            </div>
+            <div className='mx-2 mb-2 border-b border-gray-d9' />
+            <ul className='flex flex-col gap-2'>
+              {dashboards.map((dashboard) => (
+                <DashboardItem key={dashboard.id} dashboard={dashboard} nowDashboard={Number(id)} />
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </aside>
   );
 }
