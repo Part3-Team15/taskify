4821cee759207bf4daa1b40973cefd866d619e0a
un0211 <un0211@kaist.ac.kr>
2024-06-27 20:19:44 +0900

    ✨ feat(#27): 비밀번호 변경 기능 구현

--- src/containers/mypage/PwdChangeForm.tsx
+++ src/containers/mypage/PwdChangeForm.tsx
@@ -1,7 +1,9 @@
-import { ChangeEvent, useState } from 'react';
+import { AxiosError } from 'axios';
+import { ChangeEvent, FormEventHandler, useState } from 'react';
 
 import ActionButton from '@/components/Button/ActionButton';
 import PwdInput from '@/components/Input/PwdInput';
+import { putPassword } from '@/services/putService';
 
 interface PasswordChangeForm {
   password: string;
@@ -15,16 +17,18 @@ interface InputError {
   newPasswordCheck?: string;
 }
 
+const INITIAL_INPUT_DATA = {
+  password: '',
+  newPassword: '',
+  newPasswordCheck: '',
+};
+
 const checkPasswordMatch = (id: string, value: string, inputData: PasswordChangeForm) =>
   (id === 'newPassword' && inputData.newPasswordCheck && value !== inputData.newPasswordCheck) ||
   (id === 'newPasswordCheck' && value !== inputData.newPassword);
 
 export default function PwdChangeForm() {
-  const [inputData, setInputData] = useState<PasswordChangeForm>({
-    password: '',
-    newPassword: '',
-    newPasswordCheck: '',
-  });
+  const [inputData, setInputData] = useState<PasswordChangeForm>(INITIAL_INPUT_DATA);
   const [inputError, setInputError] = useState<InputError>({});
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -53,8 +57,32 @@ export default function PwdChangeForm() {
     }));
   };
 
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    const putData = async () => {
+      const { password, newPassword } = inputData;
+      try {
+        await putPassword({ password, newPassword });
+        // NOTE: 전체 페이지 리로드보다 시간이 훨씬 적게 걸려서 값만 비우도록 했습니다.
+        setInputData(INITIAL_INPUT_DATA);
+        alert('비밀번호가 변경되었습니다.');
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          alert(error.response?.data.message);
+        } else if (error instanceof Error) {
+          alert(error.message);
+        } else {
+          alert('알 수 없는 오류가 발생했습니다!');
+          console.log(error);
+        }
+      }
+    };
+
+    e.preventDefault();
+    putData();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='flex flex-col gap-4 md:grow md:gap-5'>
         <div className='relative flex flex-col gap-2.5'>
           <label htmlFor='password' className='label'>
--- src/services/putService.ts
+++ src/services/putService.ts
@@ -1,5 +1,6 @@
 import instance from './axios';
 
+import { UpdatePasswordForm } from '@/types/post/UpdatePasswordForm.interface';
 import { UpdateProfileForm } from '@/types/post/UpdateProfileForm.interface';
 import { User } from '@/types/User.interface';
 
@@ -7,4 +8,8 @@ import { User } from '@/types/User.interface';
 export const putProfile = async (formData: UpdateProfileForm) => {
   const response = await instance.put<User>(`/users/me`, formData);
   return response.data;
+};
+
+export const putPassword = async (formData: UpdatePasswordForm) => {
+  return await instance.put<User>(`/auth/password`, formData);
 };
