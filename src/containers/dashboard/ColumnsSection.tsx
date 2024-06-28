8adae0e49101c2df9979d8212bce2e05799dc23c
Jiyun Kim <wayandway@kakao.com>
2024-06-29 07:34:03 +0900

    ✨ feat(#12): 컬럼추가 모달 연동

--- src/containers/dashboard/ColumnsSection.tsx
+++ src/containers/dashboard/ColumnsSection.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Column from './Column';
 
 import useFetchData from '@/hooks/useFetchData';
+import useModal from '@/hooks/useModal';
 import { getColumnsList } from '@/services/getService';
 import { ColumnsResponse } from '@/types/Column.interface';
 
@@ -11,6 +12,7 @@ interface ColumnsSectionProps {
 }
 
 export default function ColumnsSection({ id }: ColumnsSectionProps) {
+  const { openModal } = useModal();
   const {
     data: columns,
     isLoading,
@@ -34,7 +36,12 @@ export default function ColumnsSection({ id }: ColumnsSectionProps) {
         </ul>
 
         <div className='p-5'>
-          <button className='btn-violet-light mb-4 h-[70px] w-full rounded-[6px] py-[24px] lg:mb-0 lg:w-[354px]'>
+          <button
+            className='btn-violet-light mb-4 h-[70px] w-full rounded-[6px] py-[24px] lg:mb-0 lg:w-[354px]'
+            onClick={() => {
+              openModal({ type: 'newColumn', modalProps: { dashboardId: Number(id) } });
+            }}
+          >
             <div className='mr-[12px] text-lg font-bold text-black-33'>새로운 컬럼 추가하기</div>
             <Image src='/icons/plus-filled.svg' width={22} height={22} alt='카드 추가 아이콘' loading='lazy' />
           </button>
