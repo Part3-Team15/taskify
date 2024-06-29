f1bccbbeb7cfc64106647cdce392009554386b93
un0211 <un0211@kaist.ac.kr>
2024-06-29 13:49:05 +0900

    ✨ feat(#23): 페이지네이션 텍스트 없이 사용 가능하게 변경

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -3,15 +3,16 @@ import NavButton from '../Button/NavButton';
 interface PaginationProps {
   currentChunk: number;
   totalPage: number;
+  hideText?: boolean;
   onNextClick: () => void;
   onPrevClick: () => void;
 }
 
-export default function Pagination({ currentChunk, totalPage, onNextClick, onPrevClick }: PaginationProps) {
+export default function Pagination({ currentChunk, totalPage, hideText, onNextClick, onPrevClick }: PaginationProps) {
   return (
     <div className='flex items-center justify-end'>
       {/* NOTE: 민재님꺼 pt-3 있었음 */}
-      <span className='pr-3 text-xs text-black-33 md:pr-4 md:text-sm'>
+      <span className={`pr-3 text-xs text-black-33 md:pr-4 md:text-sm ${hideText ? 'hidden' : ''}`}>
         {totalPage} 페이지 중 {currentChunk}
       </span>
 
