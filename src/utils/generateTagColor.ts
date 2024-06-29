21ff383a0ea9818d9fb29e5948b147d97f341fd0
Jiyun Kim <wayandway@kakao.com>
2024-06-29 23:10:18 +0900

    🗑  remove(#127): generateTagColor 유틸함수 삭제

--- src/utils/generateTagColor.ts
+++
@@ -1,7 +0,0 @@
-const generateTagColor = (): string => {
-  const colors = ['pink', 'blue', 'green', 'orange'];
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  return colors[randomIndex];
-};
-
-export default generateTagColor;
