6a0ca2204e7c25482eecca7005ad61f84c8a892e
un0211 <un0211@kaist.ac.kr>
2024-06-30 13:47:09 +0900

    ✨ Feat: 멤버 컴포넌트  (#128)
    * 🎨 design(#24): 구성원 컴포넌트 반응형 디자인 완성
    
    * ✨ feat(#24): 구성원 불러오기와 페이지네이션 구현
    
    * ✨ feat(#24): 멤버 삭제 기능 구현

--- src/services/deleteService.ts
+++ src/services/deleteService.ts
@@ -1,6 +1,7 @@
 import instance from './axios';
 
 import { CancelInvitationInput } from '@/types/delete/CancelInvitation.interface';
+import { DeleteMemberInput } from '@/types/delete/DeleteMemberInput.interface copy';
 
 // 대시보드 삭제
 export const deleteDashboard = async (dashboardId: number) => {
@@ -10,6 +11,11 @@ export const deleteDashboard = async (dashboardId: number) => {
 // 컬럼 삭제
 export const deleteColumn = async (columnId: number) => {
   return await instance.delete(`/columns/${columnId}`);
+};
+
+// 구성원 삭제
+export const deleteMember = async ({ memberId }: DeleteMemberInput) => {
+  return await instance.delete(`/members/${memberId}`);
 };
 
 // 대시보드 초대 취소
