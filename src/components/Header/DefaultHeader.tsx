76e1144eac731d0401869fa24b958991a784a12f
un0211 <un0211@lyuhyewon-ui-MacBookAir.local>
2024-06-30 14:00:19 +0900

    🛠  fix(#9): 헤더 모바일 높이 수정

--- src/components/Header/DefaultHeader.tsx
+++ src/components/Header/DefaultHeader.tsx
@@ -6,7 +6,7 @@ interface DefaultHeaderProps {
 
 export default function DefaultHeader({ title }: DefaultHeaderProps) {
   return (
-    <header className='flex h-[70px] w-full items-center justify-between border-b border-gray-d9 bg-white px-[24px] text-black-33 md:px-[40px]'>
+    <header className='flex h-[60px] w-full items-center justify-between border-b border-gray-d9 bg-white px-[24px] text-black-33 md:h-[70px] md:px-[40px]'>
       <h1 className='text-lg font-bold md:text-xl'>{title}</h1>
       <UserMenuDropdown />
     </header>
