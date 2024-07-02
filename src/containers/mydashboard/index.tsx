653720bf1c83a57344c6f90fa3d903e6617e56c3
jmj <wjsdncl2222@gmail.com>
2024-07-02 20:14:31 +0900

    🎨 design(#171): 내 대시보드 고정된 디자인 수정

--- src/containers/mydashboard/index.tsx
+++ src/containers/mydashboard/index.tsx
@@ -3,7 +3,7 @@ import InvitedDashboardList from './InvitedDashboardList';
 
 export default function MyDashboard() {
   return (
-    <div className='flex w-full max-w-min flex-col gap-11 bg-gray-fa p-10'>
+    <div className='flex min-w-full flex-col gap-11 bg-gray-fa p-10'>
       <DashboardList />
       <InvitedDashboardList />
     </div>
