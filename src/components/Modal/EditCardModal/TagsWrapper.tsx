cfa96c6c3350c677ff6fb1daf9cccd8a99c26801
jmj <wjsdncl2222@gmail.com>
2024-07-04 04:27:03 +0900

    🛠  fix(#11): 카드 생성 모달 태그 색상 추가

--- src/components/Modal/EditCardModal/TagsWrapper.tsx
+++ src/components/Modal/EditCardModal/TagsWrapper.tsx
@@ -1,18 +1,9 @@
+import Tags from '@/components/Tags';
+
 export default function TagsWrapper({ tags, onTagClick }: { tags: string[]; onTagClick: (tag: string) => void }) {
   return (
     <div className='mt-[10px] flex flex-wrap gap-[10px]'>
-      {tags.map((tag) => (
-        <div
-          // 태그 컬러만 지정해주면 됨
-          className='cursor-pointer text-nowrap rounded-[6px] border border-gray-d9 px-1'
-          key={`tag-${tag}-key`}
-          onClick={() => {
-            onTagClick(tag);
-          }}
-        >
-          {tag}
-        </div>
-      ))}
+      <Tags tags={tags} />
     </div>
   );
 }
