ae8822cae72954ef4ba85219137e476a75336b8f
un0211 <un0211@kaist.ac.kr>
2024-07-03 23:30:16 +0900

    🎨 design(#171): 닉네임 길어도 헤더 깨지지 않게 간격 조정

--- src/components/Header/DashboardHeader/Buttons.tsx
+++ src/components/Header/DashboardHeader/Buttons.tsx
@@ -14,14 +14,14 @@ export default function Buttons({ id }: ButtonsProps) {
 
   return (
     <div className='flex gap-1.5 text-[14px] text-gray-78 md:gap-3 lg:gap-4'>
-      <Link href={`/dashboard/${id}/edit`} className='btn-white gap-2 px-3 py-1.5 md:px-4 md:py-2 lg:py-2.5'>
+      <Link href={`/dashboard/${id}/edit`} className='btn-white gap-1 px-3 py-1.5 md:py-2 lg:gap-2 lg:px-4 lg:py-2.5'>
         <Image src={settingsIcon} alt='대시보드 관리 아이콘' />
         <span className='hidden md:inline'>관리</span>
       </Link>
       <button
         type='button'
         onClick={() => openInviteMemberModal()}
-        className='btn-white gap-2 rounded-[8px] px-3 py-1.5 md:px-4 md:py-2 lg:py-2.5'
+        className='btn-white gap-1 rounded-[8px] px-3 py-1.5 md:py-2 lg:gap-2 lg:px-4 lg:py-2.5'
       >
         <Image src={plusIcon} alt='초대 아이콘' />
         <span className='hidden md:inline'>초대하기</span>
