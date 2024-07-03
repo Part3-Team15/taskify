1f2e6847f148edcef334e6060fc722a91099e149
DAEYANG LEE <108586797+oceanlee-seoul@users.noreply.github.com>
2024-07-03 18:07:39 +0900

    ✨ Feat(#82) : 로그인 상태에 따른 리다이렉션 구현 (#154)
    * ✨ feat(#82): 로그인 상태에 따른 리다이렉션 구현
    
    * 🛠  fix(#82): 리다이렉트 로직 수정
    
    ---------
    
    Co-authored-by: un0211 <un0211@kaist.ac.kr>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import Modal from '@/components/Modal';
+import Redirect from '@/components/Redirect';
 import MainLayout from '@/layouts/MainLayout';
 import { store, persistor } from '@/store/store';
 
@@ -17,17 +18,19 @@ export default function App({ Component, pageProps }: AppProps) {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <QueryClientProvider client={queryClient}>
-          <Modal />
-          <Head>
-            <title>Taskify</title>
-            <meta name='og:title' content='Taskify' />
-            <meta name='og:description' content='새로운 일정 관리 Taskify' />
-            <meta name='og:image' content='/public/images/logo_large.png' />
-          </Head>
-          <MainLayout>
-            <Component {...pageProps} />
-          </MainLayout>
-          <ReactQueryDevtools initialIsOpen={false} />
+          <Redirect>
+            <Head>
+              <title>Taskify</title>
+              <meta name='og:title' content='Taskify' />
+              <meta name='og:description' content='새로운 일정 관리 Taskify' />
+              <meta name='og:image' content='/public/images/logo_large.png' />
+            </Head>
+            <Modal />
+            <MainLayout>
+              <Component {...pageProps} />
+            </MainLayout>
+            <ReactQueryDevtools initialIsOpen={false} />
+          </Redirect>
         </QueryClientProvider>
       </PersistGate>
     </Provider>
