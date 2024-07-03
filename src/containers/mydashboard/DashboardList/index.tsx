19091a02a59df5c6b0e3c0c0c22bf13db5196b0d
jmj <wjsdncl2222@gmail.com>
2024-07-04 00:32:28 +0900

    🎨 design(#171): 내 대시보드 흰색 부분 최대 높이 지정

--- src/containers/mydashboard/DashboardList/index.tsx
+++ src/containers/mydashboard/DashboardList/index.tsx
@@ -43,7 +43,7 @@ export default function DashboardList() {
   };
 
   return (
-    <section className='grow flex-col justify-between'>
+    <section className='flex-col justify-between'>
       <ul className='grid max-w-[350px] grid-rows-1 gap-3 font-semibold text-black-33 md:min-h-[216px] md:max-w-full md:grid-cols-2 md:grid-rows-3 lg:min-h-[140px] lg:max-w-screen-lg lg:grid-cols-3'>
         <li className='h-12 w-full rounded-lg border border-gray-d9 bg-white md:h-16'>
           <button className='btn-violet-light size-full gap-4' type='button' onClick={() => openNewDashboardModal()}>
--- src/pages/mydashboard/index.tsx
+++ src/pages/mydashboard/index.tsx
@@ -5,7 +5,7 @@ import InvitedDashboardList from '@/containers/mydashboard/InvitedDashboardList'
 
 export default function MyDashboardPage() {
   return (
-    <div className='flex h-[calc(100vh-70px)] min-w-full flex-col gap-11 bg-gray-fa p-10'>
+    <div className='flex h-full min-w-full flex-col gap-11 bg-gray-fa p-10 md:h-[calc(100vh-70px)]'>
       <Head>
         <title>Taskify | 내 대시보드</title>
       </Head>
