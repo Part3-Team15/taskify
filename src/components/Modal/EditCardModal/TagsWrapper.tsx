a7407e57bccfae68532e4b5d7be9fc69aebfea45
un0211 <un0211@kaist.ac.kr>
2024-07-04 09:55:13 +0900

    🛠  fix(#11): 태그 컴포넌트 수정 및 적용(삭제 기능 동작하도록)

--- src/components/Modal/EditCardModal/TagsWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import Tags from '@/components/Tags';
-
-export default function TagsWrapper({ tags, onTagClick }: { tags: string[]; onTagClick: (tag: string) => void }) {
-  return (
-    <div className='mt-[10px] flex flex-wrap gap-[10px]'>
-      <Tags tags={tags} />
-    </div>
-  );
-}
