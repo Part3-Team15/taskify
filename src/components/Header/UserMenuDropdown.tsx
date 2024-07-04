f3336d4dac2a890bd61ecae7d07d8bbe41e9e3a2
un0211 <un0211@kaist.ac.kr>
2024-07-04 15:26:00 +0900

    ↔️  style(#155): 안쓰는 코드 제거

--- src/components/Header/UserMenuDropdown.tsx
+++ src/components/Header/UserMenuDropdown.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 
 import UserProfile from './UserProfile';
@@ -10,8 +9,6 @@ import { RootState } from '@/store/store';
 export default function UserMenuDropdown() {
   const { isOpen, dropdownRef, handleDropdownClick, handleMenuClick, handleLogoutClick } = useUserDropdown();
   const { user } = useSelector((state: RootState) => state.user);
-  const router = useRouter();
-  const { pathname } = router;
 
   if (!user) {
     // NOTE: 공유 페이지에서 보여줄 헤더
