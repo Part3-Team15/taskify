37fc2725cea17ff79e004e7b8031165cb38109b3
un0211 <un0211@kaist.ac.kr>
2024-07-06 08:56:06 +0900

    🛠  fix(#168): 리다이렉트 수정 (랜딩, 404)
    - 랜딩: 로그인 상태일때 모달 없이 바로 나의 대시보드로
    - 404: 3초 뒤 로그인 여부에 따라 랜딩 or 나의 대시보드로

--- src/components/Redirect/index.tsx
+++ src/components/Redirect/index.tsx
@@ -1,16 +1,28 @@
 import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 
 import useRedirectIfAuth from '@/hooks/useRedirectIfAuth';
 import useRedirectIfNotAuth from '@/hooks/useRedirectIfNotAuth';
+import { RootState } from '@/store/store';
 
 export default function Redirect({ children }: { children: React.ReactNode }) {
   const router = useRouter();
+  const { user } = useSelector((state: RootState) => state.user);
   const currentPath = router.pathname;
 
   const redirectIfAuth = useRedirectIfAuth();
   const redirectIfNotAuth = useRedirectIfNotAuth();
 
-  if (['/', '/signup', '/signin'].includes(currentPath)) {
+  if (currentPath === '/' && user) {
+    router.replace('/mydashboard');
+  }
+
+  if (currentPath === '/404') {
+    const nextPath = user ? '/mydashboard' : '/';
+    setTimeout(() => router.push(nextPath), 3000);
+  }
+
+  if (['/signup', '/signin'].includes(currentPath)) {
     const isRedirecting = redirectIfAuth();
     if (isRedirecting) {
       return <></>;
