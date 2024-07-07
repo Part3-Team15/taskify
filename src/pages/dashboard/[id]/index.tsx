a2ddb487f026d15a25ff2ce3765ead5fbc08606b
Jiyun Kim <wayandway@kakao.com>
2024-07-08 00:04:45 +0900

    🎨 design(#166): 컬럼섹션 스켈레톤 수정

--- src/pages/dashboard/[id]/index.tsx
+++ src/pages/dashboard/[id]/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head';
 import { useRouter } from 'next/router';
 
+import ColumnSkeleton from '@/containers/dashboard/ColumnSkeleton';
 import ColumnsSection from '@/containers/dashboard/ColumnsSection';
 
 function DashboardIdPage() {
@@ -8,7 +8,7 @@ function DashboardIdPage() {
   const { id } = router.query;
 
   if (typeof id !== 'string') {
-    return <div>Loading...</div>; // id가 없는 경우 또는 id가 string이 아닌 경우 로딩 상태를 표시합니다.
+    return <ColumnSkeleton />;
   }
 
   return <ColumnsSection dashboardId={id} />;
