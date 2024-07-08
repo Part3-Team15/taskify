cf10d120100b459a0b0cf4b8d469ca94c3039098
Jiyun Kim <wayandway@kakao.com>
2024-07-09 02:40:01 +0900

    🎨 design(#166): 대시보드 페이지, 컬럼 섹션에 로딩 스피너 적용

--- src/pages/dashboard/[id]/index.tsx
+++ src/pages/dashboard/[id]/index.tsx
@@ -1,6 +1,6 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 
-import ColumnSkeleton from '@/containers/dashboard/ColumnSkeleton';
 import ColumnsSection from '@/containers/dashboard/ColumnsSection';
 
 function DashboardIdPage() {
@@ -8,7 +8,13 @@ function DashboardIdPage() {
   const { id } = router.query;
 
   if (typeof id !== 'string') {
-    return <ColumnSkeleton />;
+    return (
+      <div className='flex h-screen items-center justify-center'>
+        <div className='align-center opacity-50 invert dark:invert-0'>
+          <Image src='/icons/spinner.svg' alt='스피너 아이콘' width={50} height={50} />
+        </div>
+      </div>
+    );
   }
 
   return <ColumnsSection dashboardId={id} />;
