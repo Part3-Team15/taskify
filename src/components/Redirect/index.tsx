781d08e71a243be7b419a8fed5a772257254ffd1
un0211 <un0211@kaist.ac.kr>
2024-07-10 13:37:04 +0900

    💬 comment(#238): components/Redirect.tsx 주석 추가

--- src/components/Redirect/index.tsx
+++ src/components/Redirect/index.tsx
@@ -5,6 +5,7 @@ import useRedirectIfAuth from '@/hooks/useRedirectIfAuth';
 import useRedirectIfNotAuth from '@/hooks/useRedirectIfNotAuth';
 import { RootState } from '@/store/store';
 
+// NOTE: 권한에 따라 페이지 이동
 export default function Redirect({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const { user } = useSelector((state: RootState) => state.user);
@@ -13,15 +14,18 @@ export default function Redirect({ children }: { children: React.ReactNode }) {
   const redirectIfAuth = useRedirectIfAuth();
   const redirectIfNotAuth = useRedirectIfNotAuth();
 
+  // NOTE: 로그인 한 경우 mydashboard를 default로
   if (currentPath === '/' && user) {
     router.replace('/mydashboard');
   }
 
+  // NOTE: 잘못된 주소 접근 시 3초 뒤 적절한 페이지로 이동
   if (currentPath === '/404') {
     const nextPath = user ? '/mydashboard' : '/';
     setTimeout(() => router.push(nextPath), 3000);
   }
 
+  // NOTE: 로그인한 사용자가 로그인/회원가입 접근 시 mydashboard로 이동
   if (['/signup', '/signin'].includes(currentPath)) {
     const isRedirecting = redirectIfAuth();
     if (isRedirecting) {
@@ -29,6 +33,7 @@ export default function Redirect({ children }: { children: React.ReactNode }) {
     }
   }
 
+  // TODO: 추후 리팩토링 예정
   if (['/mypage', '/mydashboard'].includes(currentPath)) {
     const isRedirecting = redirectIfNotAuth();
     if (isRedirecting) {
