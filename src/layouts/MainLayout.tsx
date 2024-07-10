9403295843de85a0d5bac0fa9abefebd9657a556
un0211 <un0211@kaist.ac.kr>
2024-07-10 11:27:48 +0900

    💬 comment(#238): layout/MainLayout 주석 추가
    다른 케이스에 포함되는 404 비로그인 케이스 코드 삭제

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -11,6 +11,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   const router = useRouter();
   const currentPath = router.pathname;
 
+  // NOTE: 랜딩페이지는 헤더와 내용을 가짐
   if (currentPath === '/') {
     return (
       <div className='min-h-screen min-w-[375px]'>
@@ -20,19 +21,15 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
     );
   }
 
+  // NOTE: 로그인/회원가입은 내용만 가짐
   if (currentPath === '/signin' || currentPath === '/signup') {
     return <div className='min-h-screen min-w-[375px]'>{children}</div>;
   }
 
-  if (currentPath === '/404' && !user) {
-    return (
-      <div className='flex min-h-screen min-w-[375px] flex-col'>
-        <Header />
-        {children}
-      </div>
-    );
-  }
-
+  /* NOTE: 나머지(내 대시보드, 계정관리, 대시보드, 대시보드 관리, 404) 페이지는
+   * 로그인 한 경우: 헤더/사이드바/내용을 가짐 + 초대알림
+   * 로그인 하지 않은 경우: 헤더/내용을 가짐 (대시보드, 404만 가능)
+   */
   return (
     <div className='flex min-h-screen min-w-[375px]'>
       {user && (
