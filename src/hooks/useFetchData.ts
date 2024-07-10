f34bc4a21e709409384b3a5feab98500bef6da9b
un0211 <un0211@kaist.ac.kr>
2024-07-10 11:43:06 +0900

    💬 comment(#238): hooks/useFetchData.ts 주석 추가

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,25 +1,24 @@
 import { useQuery, QueryKey, UseQueryResult } from '@tanstack/react-query';
 
+// TODO: 인자 개수가 많아져 객체 형식으로 받는 게 좋을 듯 하나, 많은 코드 수정이 요구됨
 const useFetchData = <T>(
   queryKey: QueryKey,
   getService: () => Promise<{ data: T }>,
   refetchInterval: false | number = false,
   enabled: boolean = true,
-  handleSuccess?: () => void,
 ): UseQueryResult<T, Error> => {
   return useQuery<T, Error>({
     queryKey: queryKey,
     queryFn: async () => {
       try {
         const response = await getService();
-        handleSuccess && handleSuccess();
         return response.data;
       } catch (error) {
         throw new Error('데이터를 불러오는 중 에러 발생: ' + error);
       }
     },
-    retry: 1,
-    refetchInterval: refetchInterval,
+    retry: 1, // NOTE: 요청 실패시 1회까지만 재시도하도록 제한
+    refetchInterval: refetchInterval, // NOTE: 주기적인 초대내역 수신을 위해 설정
     enabled: enabled,
   });
 };
